8b732e71c2f4be688f92897749e6389ca6738b23
Shagun <sshagunsodhani@gmail.com>
2015-03-30 08:44:47 +0530

    Added module to generate summary using title by searching on wikipedia

--- art.py
+++ art.py
@@ -1,5 +1,6 @@
 import time
 import re
+import nltk
 
 try:
     import wikipedia as wiki
@@ -14,9 +15,10 @@ except ImportError as exc:
 
 class art():
 
-	def __init__(self, title, artist, verbose = 0, stopwords = {}, artist_summary_size = 4, title_summary_size = 4):
+	def __init__(self, title, artist, object_number, verbose = 0, stopwords = {}, artist_summary_size = 4, title_summary_size = 4):
 		self.artist = artist
 		self.title = title
+		self.object_number = object_number
 		self.verbose = verbose
 		self.info = 0
 		self.stopwords = stopwords
@@ -26,39 +28,59 @@ class art():
 	def describe_title(self):
 		(wiki_title_results, wiki_title_suggestions) = wiki.search(self.title, suggestion = True)
 		if len(wiki_title_results) > 0:
-			a = wiki_title_results[0]
+			wiki_id = wiki_title_results[0]
 		elif wiki_title_suggestions!=None:
-			a = wiki_title_suggestions[0]
+			wiki_id = wiki_title_suggestions[0]
 		else:
 			return 0
-		# print type(a)
-		# print self.title
-		summary = wiki.summary(a)
-		summary_word_list = summary.split()
+		page = wiki.page(wiki_id)
+		ss = SimpleSummarizer()
+		porter_stemmer = nltk.stem.porter.PorterStemmer()
+		wordnet_lemmatizer = nltk.stem.WordNetLemmatizer()	
+		summary = page.summary
+		summary_word_list_temp = summary.split()
+		summary_word_list = []
+		for i in summary_word_list_temp:
+			token = wordnet_lemmatizer.lemmatize(porter_stemmer.stem(i.lower()))
+			summary_word_list.append(token)
+
 		summary_size = len(summary_word_list)
 		print "summary : "
 		print summary
 		print "title : "
 		print self.title
-		flag = 0
 		if (summary_size > 30):
-			# print summary
-			# print "summary size > 30"
 			title_word_list = self.title.split()
+			# print title_word_list
 			for i in title_word_list:
-				if i not in self.stopwords:
-					if i not in summary_word_list:
-						flag = -1
-						print i
-						# break;
-
-		# else:
-			#summary too small
-			#try generating summary using artist name
-
+				a = i.encode('ascii','ignore')
+				token = wordnet_lemmatizer.lemmatize(porter_stemmer.stem(a.strip().lower()))
+				token = token.encode('ascii','ignore')
+				count = 0				
+				if token not in self.stopwords:
+					if token not in summary_word_list and token not in summary_word_list_temp:
+						count+=1
+			if count < 0.4*len(title_word_list):
+				#summary accepted
+				print "summary accepted"			
+				content = ""
+				a = re.split('\n==[A-Za-z ]*==\n', page.content.split('== References ==')[0])
+				for i in a:
+					content+=i.strip()+"\n"
+				if self.verbose:
+					print "Wikipedia Summary about the art-piece : ", page.summary
+					print "\n"
+					print "Generated Summary about the art-peice : ", ss.summarize(content, self.title_summary_size)
+					print "\n"
+					print "Content about the art-peice : ", content
+					print "\n"
+		else:
+			print "Scrape karenge!"
+						
 
 
 	def describe_artist(self):
+
 		if self.verbose:
 			print "Generating information for artist ", self.artist
 		wiki_artist_results = wiki.search(self.artist)
@@ -72,9 +94,15 @@ class art():
 		a = re.split('\n==[A-Za-z ]*==\n', page.content.split('== References ==')[0])
 		for i in a:
 			content+=i.strip()+"\n"
-		print "Wikipedia Summary about the artist : ", page.summary
-		print "\n"
-		print "Generated Summary about the artist : ", ss.summarize(content, self.artist_summary_size)
-		print "\n"
-		print "Content about the artist : ", content
-		print "\n"
+		if self.verbose:
+			print "Wikipedia Summary about the artist : ", page.summary
+			print "\n"
+			print "Generated Summary about the artist : ", ss.summarize(content, self.artist_summary_size)
+			print "\n"
+			print "Content about the artist : ", content
+			print "\n"
+		for i in page.links:
+			print i
+
+		print self.title
+		# print page.links
